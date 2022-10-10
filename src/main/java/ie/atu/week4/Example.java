983c8225fff5c458f966499fc2e72425ea04e41c
sandrius <G00402163@atu.ie>
2022-10-10 10:59:15 +0100

    made new F_delete

--- src/main/java/ie/atu/week4/Example.java
+++ src/main/java/ie/atu/week4/Example.java
@@ -7,18 +7,19 @@ public class Example
 {
     public static void main(String[] args)
     {
-        System.out.println("Good Morning");
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter a number");
-        int firstNumber = input.nextInt();
-        System.out.println("You entered : " + firstNumber);
-        add();
-
+        subtract();
     }
 
-    //This method takes in a value and adds
-    public static void add()
+    public static void subtract()
     {
-        System.out.println("You just called a method");
+        System.out.println("Write your first number :");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Write your secondNumber :");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber - secondNumber;
+        System.out.println("The total is " + total);
     }
 }
