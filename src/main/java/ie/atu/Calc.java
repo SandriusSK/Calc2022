4d60e8c20984b0a8f3429e0fd44c124163c9b783
sandrius <G00402163@atu.ie>
2022-10-10 11:02:42 +0100

    made new F_Subtract

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Calc {
     public static void main(String[] args) {
         add();
+        subtract();
 
     }
 
@@ -17,6 +18,19 @@ public class Calc {
         int secondNumber = inputs.nextInt();
 
         int total = firstNumber + secondNumber;
+        System.out.println("The total is " + total);
+    }
+
+    public static void subtract()
+    {
+        System.out.println("Write your first number :");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Write your secondNumber :");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber - secondNumber;
         System.out.println("The total is " + total);
     }
 }
