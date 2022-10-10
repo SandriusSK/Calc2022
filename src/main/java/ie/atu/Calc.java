cab7c6e53c9c4d751f5b2321a7e7cea0dafa4f1f
sandrius <G00402163@atu.ie>
2022-10-10 10:54:24 +0100

    made new F_add

---
+++ src/main/java/ie/atu/Calc.java
@@ -0,0 +1,23 @@
+package ie.atu;
+
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        add();
+
+    }
+
+    public static void add() {
+        System.out.println("Write your first number :");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Write your secondNumber :");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber + secondNumber;
+        System.out.println("The total is " + total);
+    }
+}
+
