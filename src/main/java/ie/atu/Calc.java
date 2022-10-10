d66dc68cccc933a2f0576ee66a16eae0030405a6
sandrius <G00402163@atu.ie>
2022-10-10 11:07:51 +0100

    made new F_multiply

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -6,6 +6,7 @@ public class Calc {
     public static void main(String[] args) {
         add();
         subtract();
+        multiply();
 
     }
 
@@ -21,8 +22,7 @@ public class Calc {
         System.out.println("The total is " + total);
     }
 
-    public static void subtract()
-    {
+    public static void subtract() {
         System.out.println("Write your first number :");
         Scanner inputs = new Scanner(System.in);
         int firstNumber = inputs.nextInt();
@@ -31,6 +31,18 @@ public class Calc {
         int secondNumber = inputs.nextInt();
 
         int total = firstNumber - secondNumber;
+        System.out.println("The total is " + total);
+    }
+
+    public static void multiply() {
+        System.out.println("Write your first number :");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Write your secondNumber :");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
         System.out.println("The total is " + total);
     }
 }
